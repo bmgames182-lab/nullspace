3e4e65335cf4d3ef9f33039f1529e458bff7856b
bmgames182-lab <bmgames182@gmail.com>
2026-09-12 10:18:46 +0530

    Run persistent v9 coping controller in lab

--- client/biological_human_v6.js
+++ client/biological_human_v6.js
@@ -1,3 +1,3 @@
 // Compatibility bridge: existing runtime/tests import the v6 symbol, while the
-// current implementation lives in v8 with conscious distress and protection.
-export { BiologicalArtagdollHumanV8 as BiologicalArtagdollHumanV6 } from "./biological_human_v8.js";
+// current implementation lives in v9 with persistent conscious injury coping.
+export { BiologicalArtagdollHumanV9 as BiologicalArtagdollHumanV6 } from "./biological_human_v9.js";
